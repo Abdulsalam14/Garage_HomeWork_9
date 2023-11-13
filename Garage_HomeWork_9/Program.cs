0c43d561e0cc2b5f892e5f7db7aa0fb7d0d33539
Abdulsalam14 <abdullayevabdulsalam@gmail.com>
2023-11-13 23:27:14 +0400

    Add project files.

---
+++ Garage_HomeWork_9/Program.cs
@@ -0,0 +1,24 @@
+﻿
+int[] arr = new int[0];
+arr = arr.Add(1);
+arr = arr.Add(2);
+arr = arr.Add(3);
+arr = arr.Add(4);
+int sum = 0;
+foreach (var item in arr)
+{
+    sum += item;
+}
+Console.WriteLine(sum);
+
+
+
+public static class Extension
+{
+    public static int[] Add(this int[] arr, int i)
+    {
+        Array.Resize(ref arr, arr.Length + 1);
+        arr[arr.Length - 1] = i;
+        return arr;
+    }
+}
